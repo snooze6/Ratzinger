0b9a3fc1f9fbd5a13a7606ee6d4c71e4c42194f4
Armando <armandonogueirar@gmail.com>
2016-04-01 19:20:50 +0400

    JDNI

--- src/main/java/rc/diego/model/persistence/Connector/MySqlConnector.java
+++ src/main/java/rc/diego/model/persistence/Connector/MySqlConnector.java
@@ -1,39 +1,29 @@
 package rc.diego.model.persistence.Connector;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 
 /**
  * Created by entakitos on 1/03/16.
  */
 public class MySqlConnector implements InterfaceConnector{
-    private String host="ratztest.cdh6gtvsyebq.us-east-1.rds.amazonaws.com";
-    private String database="ratztest";
-    private String user="ratztest";
-    private String passwd="ratztest";
 
-//    private String host="localhost";
-//    private String database="dawa_04_test";
-//    private String user="root";
-//    private String passwd="root";
+    public Connection getConnection()throws Exception{
+        try {
+            Context ctx = new InitialContext();
+            return ((DataSource)ctx.lookup("java:comp/env/jdbc/amazon")).getConnection();
 
-
-    public Connection getConnection()throws Exception
-    {
-        String url = "";
-        try
-        {
-            url = "jdbc:mysql://"+host+"/"+database+"?user="+user+"&password="+passwd;
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(url);
-            System.out.println("Conexion establecida con " + url + "...");
-            return con;
-        }
-        catch (java.sql.SQLException e)
-        {
-            System.out.println("Conexion NO establecida con " + url);
+        } catch (NamingException e) {
+            e.printStackTrace();
+            System.out.println("Conexion NO establecida");
+        } catch (java.sql.SQLException e) {
+            System.out.println("Conexion NO establecida");
             throw (e);
         }
+        return null;
     }
 
 }
