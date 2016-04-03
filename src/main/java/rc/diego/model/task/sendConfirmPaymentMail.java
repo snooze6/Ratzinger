112e17a3c1100117dd3b35f6fd2af3e70c888209
Diego Reiriz <diegoreiriz@gmail.com>
2016-04-03 18:52:33 +0200

    os precios das compras almacenanse na base de datos cos descontos aplicados

---
+++ src/main/java/rc/diego/model/task/sendConfirmPaymentMail.java
@@ -0,0 +1,40 @@
+package rc.diego.model.task;
+
+import rc.diego.model.VO.VOShoppingCart;
+import rc.diego.model.VO.VOUser;
+import rc.diego.model.utils.mail.JavaMail;
+
+/**
+ * Created by entakitos on 3/04/16.
+ */
+public class sendConfirmPaymentMail implements InterfaceTask{
+
+    private VOUser user;
+    private VOShoppingCart cart;
+
+    public VOUser getUser() {
+        return user;
+    }
+
+    public void setUser(VOUser user) {
+        this.user = user;
+    }
+
+    public VOShoppingCart getCart() {
+        return cart;
+    }
+
+    public void setCart(VOShoppingCart cart) {
+        this.cart = cart;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public sendConfirmPaymentMail(VOUser user, VOShoppingCart cart) {
+        this.user = user;
+        new JavaMail().sendMail(user.geteMail());
+    }
+}
---
+++ src/main/java/rc/diego/model/utils/mail/JavaMail.java
@@ -0,0 +1,65 @@
+package rc.diego.model.utils.mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+/**
+ * Created by entakitos on 3/04/16.
+ */
+
+public class JavaMail {
+
+    private final String from = "dawatest4@gmail.com";
+    private final String username = "4testdawa";//change accordingly
+    private final String password = "4testdawa";//change accordingly
+
+    private final Properties props = new Properties();
+
+    public void sendMail(String to) {
+        // Recipient's email ID needs to be mentioned.
+        to = "destinationemail@gmail.com";
+
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JavaMail() {
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+
+    }
+}
